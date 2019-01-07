4dd73bf435e96abb8faf4aff0491094f91c19aec
Kyle Sekellick <sekellickkyle@yahoo.com>
2019-01-07 02:58:49 -0500

    adds finished scraper code and current testing index scraping code

--- index.js
+++ index.js
@@ -1,36 +1,56 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+
+    const extractPartners = async (url) => {
+        const page = await browser.newPage();
+        await page.goto(url);
+        // await page.screenshot({path: 'example.png'});
+        
+        // way to scrape multiple items and create an object
+        const partnersOnPage = await page.evaluate(() => 
+            Array.from(document.querySelectorAll('div.compact'))
+                .map(compacts => ({
+                    title: compacts.querySelector('h3.title').innerText.trim(),
+                    logo: compacts.querySelector('.logo img').src
+                }))
+        );
+        
+        await page.close();
+
+        // Should we end recursion?
+        if (partnersOnPage.length < 1) {
+            console.log(`terminate recursion on: ${url}`);
+            return partnersOnPage;
+        } else {
+            // Go fetch the next page
+            // What is the next URL?
+            const nextPageNumber = parseInt(url.match(/page=(\d+)$/)[1], 10) + 1;
+            const nextUrl = `https://marketingplatform.google.com/about/partners/find-a-partner?page=${nextPageNumber}`;
+            
+            return partnersOnPage.concat(await extractPartners(nextUrl));
+        }
+    }
+    
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const url = 'https://marketingplatform.google.com/about/partners/find-a-partner?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fpartners%2F';
-    await page.goto(url);
-    // await page.screenshot({path: 'example.png'});
 
-    // h3 scrape
-    const titles = await page.evaluate(() => 
-        Array.from(document.querySelectorAll('div.compact h3.title'))
-            .map(partner => partner.innerText.trim())
-    );
-    
-    // image scrape
-    const logos = await page.evaluate(() => 
-        Array.from(document.querySelectorAll('div.compact .logo img'))
-            .map(logo => logo.src)
-    );
-    
-    // way to scrape multiple items and create an object
-    const partners = await page.evaluate(() => 
-        Array.from(document.querySelectorAll('div.compact'))
-            .map(compacts => ({
-                title: compacts.querySelector('h3.title').textContent.trim(),
-                logo: compacts.querySelector('.logo img').src
-            }))
-    );
-
+        // h3 scrape
+    // const titles = await page.evaluate(() => 
+    //     Array.from(document.querySelectorAll('div.compact h3.title'))
+    //         .map(partner => partner.innerText.trim())
+    // );
     // console.log(titles);
+    
+        // image scrape
+    // const logos = await page.evaluate(() => 
+    //     Array.from(document.querySelectorAll('div.compact .logo img'))
+    //         .map(logo => logo.src)
+    // );
     // console.log(logos);
-    console.log(partners);
+
+    const firstUrl = 'https://marketingplatform.google.com/about/partners/find-a-partner?page=1';
+    const partners = await extractPartners(firstUrl);
+    // console.log(partners);
 
     await browser.close();
 })();
