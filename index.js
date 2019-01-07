fde06b2255dbdc16bb65507c87e0bc02bd5eab44
Kyle Sekellick <sekellickkyle@yahoo.com>
2019-01-06 23:17:15 -0500

    adds code that scrapes for specific text content

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     const url = 'https://marketingplatform.google.com/about/partners/find-a-partner?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fpartners%2F';
     await page.goto(url);
-    await page.screenshot({path: 'example.png'});
+    // await page.screenshot({path: 'example.png'});
+
+    const h2 = await page.evaluate(() => document.querySelector('h2').textContent);
+    // const innerText = await page.evaluate(() => document.querySelector('p').innerText);
+    
+    console.log(h2);
+    // console.log(innerText);
 
     await browser.close();
 })();
